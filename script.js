e57f4b2d16e1c505bf21d9caa54705bdb353f579
Jim Lin-Dybner <lin.jycl@gmail.com>
2023-05-15 11:15:31 -0400

    minor formatting adjustments for readability in script.

--- script.js
+++ script.js
@@ -30,7 +30,6 @@ window.onload = () => {
             source: "Making Numbers Count: The Art and Science of Communicating Numbers",
             sourceURL: "https://www.simonandschuster.com/books/Making-Numbers-Count/Chip-Heath/9781982165444"
         },
-        
         {
             desc: "Naturally, twins occur in about 1 in 250 pregnancies",
             numer: 1,
@@ -38,7 +37,6 @@ window.onload = () => {
             source: "ReproductiveFacts.org",
             sourceURL: "https://www.reproductivefacts.org/news-and-publications/patient-fact-sheets-and-booklets/documents/fact-sheets-and-info-booklets/multiple-pregnancy-and-birth-twins-triplets-and-high-order-multiples-booklet/#:~:text=Naturally%2C%20twins%20occur%20in%20about,but%20there%20are%20other%20factors."
         },
-        
         {
             desc: "About 1 in every 3,000 calico cats is born a male.",
             numer: 1,
@@ -46,7 +44,6 @@ window.onload = () => {
             source: "The Spruce Pets",
             sourceURL: "https://www.thesprucepets.com/calico-cats-photo-gallery-4031810#:~:text=About%20one%20in%20every%203%2C000,of%20many%20other%20health%20problems."
         },
-        
         {
             desc: "Your chance of getting the Royal Flush hand in a game of Texas Hold'em is 1 in 649,740.",
             numer: 1,
@@ -54,7 +51,6 @@ window.onload = () => {
             source: "Wikipedia",
             sourceURL: "https://en.wikipedia.org/wiki/Poker_probability"
         },
-        
         {
             desc: "By 2050, 1 in 4 people on Earth will be African.",
             numer: 1,
@@ -62,7 +58,6 @@ window.onload = () => {
             source: "CNBC",
             sourceURL: "https://www.cnbc.com/2015/07/30/world-population-quarter-of-earth-will-be-african-in-2050.html"
         },
-        
         {
             desc: "English is spoken by about 1 in 5 people on Earth.",
             numer: 1,
@@ -70,7 +65,6 @@ window.onload = () => {
             source: "CIA World Factbook",
             sourceURL: "https://www.cia.gov/the-world-factbook/countries/world/#people-and-society"
         },
-        
         {
             desc: "Mandarin Chinese is spoken by almost 7 out of every 50 people on Earth.",
             numer: 7,
@@ -78,7 +72,6 @@ window.onload = () => {
             source: "CIA World Factbook",
             sourceURL: "https://www.cia.gov/the-world-factbook/countries/world/#people-and-society"
         },
-        
         {
             desc: "Hindi is spoken by 3 out of every 40 people on Earth.",
             numer: 3,
@@ -86,7 +79,6 @@ window.onload = () => {
             source: "CIA World Factbook",
             sourceURL: "https://www.cia.gov/the-world-factbook/countries/world/#people-and-society"
         },
-        
         {
             desc: "Spanish is spoken by almost 7 out of every 100 people on Earth.",
             numer: 7,
@@ -94,7 +86,6 @@ window.onload = () => {
             source: "CIA World Factbook",
             sourceURL: "https://www.cia.gov/the-world-factbook/countries/world/#people-and-society"
         },
-        
         {
             desc: "About 1 in 20 people on Earth speaks Arabic as their first language.",
             numer: 1,
@@ -102,7 +93,6 @@ window.onload = () => {
             source: "CIA World Factbook",
             sourceURL: "https://www.cia.gov/the-world-factbook/countries/world/#people-and-society"
         },
-        
         {
             desc: "About 1 in 3 people in the world is Christian.",
             numer: 1,
@@ -110,7 +100,6 @@ window.onload = () => {
             source: "CIA World Factbook",
             sourceURL: "https://www.cia.gov/the-world-factbook/countries/world/#people-and-society"
         },
-        
         {
             desc: "About 1 in 4 people in the world is Muslim.",
             numer: 1,
@@ -118,7 +107,6 @@ window.onload = () => {
             source: "CIA World Factbook",
             sourceURL: "https://www.cia.gov/the-world-factbook/countries/world/#people-and-society"
         },
-        
         {
             desc: "About 3 out of every 20 people in the world are Hindus.",
             numer: 3,
@@ -126,7 +114,6 @@ window.onload = () => {
             source: "CIA World Factbook",
             sourceURL: "https://www.cia.gov/the-world-factbook/countries/world/#people-and-society"
         },
-        
         {
             desc: "Almost 3 in 5 people on earth live in cities.",
             numer: 3,
@@ -134,7 +121,6 @@ window.onload = () => {
             source: "CIA World Factbook",
             sourceURL: "https://www.cia.gov/the-world-factbook/countries/world/#people-and-society"
         },
-        
         {
             desc: "About 1 in 4 people on earth is in the 0&ndash;14 years age range",
             numer: 1,
@@ -142,7 +128,6 @@ window.onload = () => {
             source: "CIA World Factbook",
             sourceURL: "https://www.cia.gov/the-world-factbook/countries/world/#people-and-society"
         },
-        
         {
             desc: "More than 1 in 3 people on earth is in the 15&ndash;64 years age range",
             numer: 1,
@@ -150,7 +135,6 @@ window.onload = () => {
             source: "CIA World Factbook",
             sourceURL: "https://www.cia.gov/the-world-factbook/countries/world/#people-and-society"
         },
-        
         {
             desc: "About 1 in 10 people on earth is older than 65.",
             numer: 1,
@@ -158,7 +142,6 @@ window.onload = () => {
             source: "CIA World Factbook",
             sourceURL: "https://www.cia.gov/the-world-factbook/countries/world/#people-and-society"
         },
-        
         {
             desc: "About 2 in 5 Canadians live in the province of Ontario.",
             numer: 2,
@@ -166,7 +149,6 @@ window.onload = () => {
             source: "Google",
             sourceURL: "https://www.google.com/search?q=ontario+population&oq=ontario+population&aqs=chrome.0.0i67i433i650j0i67i650j0i131i433i512j0i512l2j69i64j0i512l2.1853j1j4&sourceid=chrome&ie=UTF-8"
         },
-        
         {
             desc: "In 2011, about 3 in 10 Canadians spoke French.",
             numer: 3,
@@ -174,7 +156,6 @@ window.onload = () => {
             source: "Wikipedia",
             sourceURL: "https://en.wikipedia.org/wiki/Francophone_Canadians"
         },
-        
         {
             desc: "About 21 out of 25 Brits live in England.",
             numer: 21,
