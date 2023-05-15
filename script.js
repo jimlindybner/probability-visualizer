bfc2c53821b7fdccd28aed4271cc787f9d946e57
Jim Lin-Dybner <lin.jycl@gmail.com>
2023-05-15 11:04:54 -0400

    array of objects refactored.

--- script.js
+++ script.js
@@ -22,161 +22,167 @@ window.onload = () => {
     formNumerator.focus();
 
     // random stats & facts
-    let funStats = [];
-    let handWash = {
-        desc: "40%, or 2 in 5, of the people you shook hands with didn't wash their hands after the last time they used the washroom.",
-        numer: 2,
-        denom: 5,
-        source: "Making Numbers Count: The Art and Science of Communicating Numbers",
-        sourceURL: "https://www.simonandschuster.com/books/Making-Numbers-Count/Chip-Heath/9781982165444"
-    }
-
-    let twins = {
-        desc: "Naturally, twins occur in about 1 in 250 pregnancies",
-        numer: 1,
-        denom: 250,
-        source: "ReproductiveFacts.org",
-        sourceURL: "https://www.reproductivefacts.org/news-and-publications/patient-fact-sheets-and-booklets/documents/fact-sheets-and-info-booklets/multiple-pregnancy-and-birth-twins-triplets-and-high-order-multiples-booklet/#:~:text=Naturally%2C%20twins%20occur%20in%20about,but%20there%20are%20other%20factors."
-    }
-
-    let calicoMale = {
-        desc: "About 1 in every 3,000 calico cats is born a male.",
-        numer: 1,
-        denom: 3000,
-        source: "The Spruce Pets",
-        sourceURL: "https://www.thesprucepets.com/calico-cats-photo-gallery-4031810#:~:text=About%20one%20in%20every%203%2C000,of%20many%20other%20health%20problems."
-    }
-
-    let royalFlush = {
-        desc: "Your chance of getting the Royal Flush hand in a game of Texas Hold'em is 1 in 649,740.",
-        numer: 1,
-        denom: 649740,
-        source: "Wikipedia",
-        sourceURL: "https://en.wikipedia.org/wiki/Poker_probability"
-    }
-
-    let african2050 = {
-        desc: "By 2050, 1 in 4 people on Earth will be African.",
-        numer: 1,
-        denom: 4,
-        source: "CNBC",
-        sourceURL: "https://www.cnbc.com/2015/07/30/world-population-quarter-of-earth-will-be-african-in-2050.html"
-    }
-
-    let englishLang = {
-        desc: "English is spoken by about 1 in 5 people on Earth.",
-        numer: 1,
-        denom: 5,
-        source: "CIA World Factbook",
-        sourceURL: "https://www.cia.gov/the-world-factbook/countries/world/#people-and-society"
-    }
-
-    let mandarinLang = {
-        desc: "Mandarin Chinese is spoken by almost 7 out of every 50 people on Earth.",
-        numer: 7,
-        denom: 50,
-        source: "CIA World Factbook",
-        sourceURL: "https://www.cia.gov/the-world-factbook/countries/world/#people-and-society"
-    }
-
-    let hindiLang = {
-        desc: "Hindi is spoken by 3 out of every 40 people on Earth.",
-        numer: 3,
-        denom: 40,
-        source: "CIA World Factbook",
-        sourceURL: "https://www.cia.gov/the-world-factbook/countries/world/#people-and-society"
-    }
-
-    let spanishLang = {
-        desc: "Spanish is spoken by almost 7 out of every 100 people on Earth.",
-        numer: 7,
-        denom: 100,
-        source: "CIA World Factbook",
-        sourceURL: "https://www.cia.gov/the-world-factbook/countries/world/#people-and-society"
-    }
-
-    let arabicLang = {
-        desc: "About 1 in 20 people on Earth speaks Arabic as their first language.",
-        numer: 1,
-        denom: 20,
-        source: "CIA World Factbook",
-        sourceURL: "https://www.cia.gov/the-world-factbook/countries/world/#people-and-society"
-    }
-
-    let christianity = {
-        desc: "About 1 in 3 people in the world is Christian.",
-        numer: 1,
-        denom: 3,
-        source: "CIA World Factbook",
-        sourceURL: "https://www.cia.gov/the-world-factbook/countries/world/#people-and-society"
-    }
-
-    let islam = {
-        desc: "About 1 in 4 people in the world is Muslim.",
-        numer: 1,
-        denom: 4,
-        source: "CIA World Factbook",
-        sourceURL: "https://www.cia.gov/the-world-factbook/countries/world/#people-and-society"
-    }
-
-    let hinduism = {
-        desc: "About 3 out of every 20 people in the world are Hindus.",
-        numer: 3,
-        denom: 20,
-        source: "CIA World Factbook",
-        sourceURL: "https://www.cia.gov/the-world-factbook/countries/world/#people-and-society"
-    }
-
-    let urbanization = {
-        desc: "Almost 3 in 5 people on earth live in cities.",
-        numer: 3,
-        denom: 5,
-        source: "CIA World Factbook",
-        sourceURL: "https://www.cia.gov/the-world-factbook/countries/world/#people-and-society"
-    }
-
-    let children = {
-        desc: "About 1 in 4 people on earth is in the 0&ndash;14 years age range",
-        numer: 1,
-        denom: 4,
-        source: "CIA World Factbook",
-        sourceURL: "https://www.cia.gov/the-world-factbook/countries/world/#people-and-society"
-    }
-
-    let primeAge = {
-        desc: "More than 1 in 3 people on earth is in the 15&ndash;64 years age range",
-        numer: 1,
-        denom: 3,
-        source: "CIA World Factbook",
-        sourceURL: "https://www.cia.gov/the-world-factbook/countries/world/#people-and-society"
-    }
-
-    let seniorCitizens = {
-        desc: "About 1 in 10 people on earth is older than 65.",
-        numer: 1,
-        denom: 10,
-        source: "CIA World Factbook",
-        sourceURL: "https://www.cia.gov/the-world-factbook/countries/world/#people-and-society"
-    }
-
-    let canadiansInOntario = {
-        desc: "About 2 in 5 Canadians live in the province of Ontario.",
-        numer: 2,
-        denom: 5,
-        source: "Google",
-        sourceURL: "https://www.google.com/search?q=ontario+population&oq=ontario+population&aqs=chrome.0.0i67i433i650j0i67i650j0i131i433i512j0i512l2j69i64j0i512l2.1853j1j4&sourceid=chrome&ie=UTF-8"
-    }
-
-    let francophoneCanadians = {
-        desc: "In 2011, about 3 in 10 Canadians spoke French.",
-        numer: 3,
-        denom: 10,
-        source: "Wikipedia",
-        sourceURL: "https://en.wikipedia.org/wiki/Francophone_Canadians"
-    }
-    
-    // adding random stat/fact objects to funStats arr
-    funStats.push(handWash, twins, calicoMale, royalFlush, african2050, englishLang, mandarinLang, spanishLang, hindiLang, arabicLang, christianity, islam, hinduism, urbanization, children, primeAge, seniorCitizens, canadiansInOntario, francophoneCanadians);
+    let funStats = [
+        {
+            desc: "40%, or 2 in 5, of the people you shook hands with didn't wash their hands after the last time they used the washroom.",
+            numer: 2,
+            denom: 5,
+            source: "Making Numbers Count: The Art and Science of Communicating Numbers",
+            sourceURL: "https://www.simonandschuster.com/books/Making-Numbers-Count/Chip-Heath/9781982165444"
+        },
+        
+        {
+            desc: "Naturally, twins occur in about 1 in 250 pregnancies",
+            numer: 1,
+            denom: 250,
+            source: "ReproductiveFacts.org",
+            sourceURL: "https://www.reproductivefacts.org/news-and-publications/patient-fact-sheets-and-booklets/documents/fact-sheets-and-info-booklets/multiple-pregnancy-and-birth-twins-triplets-and-high-order-multiples-booklet/#:~:text=Naturally%2C%20twins%20occur%20in%20about,but%20there%20are%20other%20factors."
+        },
+        
+        {
+            desc: "About 1 in every 3,000 calico cats is born a male.",
+            numer: 1,
+            denom: 3000,
+            source: "The Spruce Pets",
+            sourceURL: "https://www.thesprucepets.com/calico-cats-photo-gallery-4031810#:~:text=About%20one%20in%20every%203%2C000,of%20many%20other%20health%20problems."
+        },
+        
+        {
+            desc: "Your chance of getting the Royal Flush hand in a game of Texas Hold'em is 1 in 649,740.",
+            numer: 1,
+            denom: 649740,
+            source: "Wikipedia",
+            sourceURL: "https://en.wikipedia.org/wiki/Poker_probability"
+        },
+        
+        {
+            desc: "By 2050, 1 in 4 people on Earth will be African.",
+            numer: 1,
+            denom: 4,
+            source: "CNBC",
+            sourceURL: "https://www.cnbc.com/2015/07/30/world-population-quarter-of-earth-will-be-african-in-2050.html"
+        },
+        
+        {
+            desc: "English is spoken by about 1 in 5 people on Earth.",
+            numer: 1,
+            denom: 5,
+            source: "CIA World Factbook",
+            sourceURL: "https://www.cia.gov/the-world-factbook/countries/world/#people-and-society"
+        },
+        
+        {
+            desc: "Mandarin Chinese is spoken by almost 7 out of every 50 people on Earth.",
+            numer: 7,
+            denom: 50,
+            source: "CIA World Factbook",
+            sourceURL: "https://www.cia.gov/the-world-factbook/countries/world/#people-and-society"
+        },
+        
+        {
+            desc: "Hindi is spoken by 3 out of every 40 people on Earth.",
+            numer: 3,
+            denom: 40,
+            source: "CIA World Factbook",
+            sourceURL: "https://www.cia.gov/the-world-factbook/countries/world/#people-and-society"
+        },
+        
+        {
+            desc: "Spanish is spoken by almost 7 out of every 100 people on Earth.",
+            numer: 7,
+            denom: 100,
+            source: "CIA World Factbook",
+            sourceURL: "https://www.cia.gov/the-world-factbook/countries/world/#people-and-society"
+        },
+        
+        {
+            desc: "About 1 in 20 people on Earth speaks Arabic as their first language.",
+            numer: 1,
+            denom: 20,
+            source: "CIA World Factbook",
+            sourceURL: "https://www.cia.gov/the-world-factbook/countries/world/#people-and-society"
+        },
+        
+        {
+            desc: "About 1 in 3 people in the world is Christian.",
+            numer: 1,
+            denom: 3,
+            source: "CIA World Factbook",
+            sourceURL: "https://www.cia.gov/the-world-factbook/countries/world/#people-and-society"
+        },
+        
+        {
+            desc: "About 1 in 4 people in the world is Muslim.",
+            numer: 1,
+            denom: 4,
+            source: "CIA World Factbook",
+            sourceURL: "https://www.cia.gov/the-world-factbook/countries/world/#people-and-society"
+        },
+        
+        {
+            desc: "About 3 out of every 20 people in the world are Hindus.",
+            numer: 3,
+            denom: 20,
+            source: "CIA World Factbook",
+            sourceURL: "https://www.cia.gov/the-world-factbook/countries/world/#people-and-society"
+        },
+        
+        {
+            desc: "Almost 3 in 5 people on earth live in cities.",
+            numer: 3,
+            denom: 5,
+            source: "CIA World Factbook",
+            sourceURL: "https://www.cia.gov/the-world-factbook/countries/world/#people-and-society"
+        },
+        
+        {
+            desc: "About 1 in 4 people on earth is in the 0&ndash;14 years age range",
+            numer: 1,
+            denom: 4,
+            source: "CIA World Factbook",
+            sourceURL: "https://www.cia.gov/the-world-factbook/countries/world/#people-and-society"
+        },
+        
+        {
+            desc: "More than 1 in 3 people on earth is in the 15&ndash;64 years age range",
+            numer: 1,
+            denom: 3,
+            source: "CIA World Factbook",
+            sourceURL: "https://www.cia.gov/the-world-factbook/countries/world/#people-and-society"
+        },
+        
+        {
+            desc: "About 1 in 10 people on earth is older than 65.",
+            numer: 1,
+            denom: 10,
+            source: "CIA World Factbook",
+            sourceURL: "https://www.cia.gov/the-world-factbook/countries/world/#people-and-society"
+        },
+        
+        {
+            desc: "About 2 in 5 Canadians live in the province of Ontario.",
+            numer: 2,
+            denom: 5,
+            source: "Google",
+            sourceURL: "https://www.google.com/search?q=ontario+population&oq=ontario+population&aqs=chrome.0.0i67i433i650j0i67i650j0i131i433i512j0i512l2j69i64j0i512l2.1853j1j4&sourceid=chrome&ie=UTF-8"
+        },
+        
+        {
+            desc: "In 2011, about 3 in 10 Canadians spoke French.",
+            numer: 3,
+            denom: 10,
+            source: "Wikipedia",
+            sourceURL: "https://en.wikipedia.org/wiki/Francophone_Canadians"
+        },
+        
+        {
+            desc: "About 21 out of 25 Brits live in England.",
+            numer: 21,
+            denom: 25,
+            source: "Google",
+            sourceURL: "https://www.google.com/search?q=population+england&oq=population+England&aqs=chrome.0.0i512l10.2347j1j9&sourceid=chrome&ie=UTF-8"
+        }
+    ];
 
     // functions
     // random stat
@@ -185,15 +191,14 @@ window.onload = () => {
         question.style.display = "none";
         btnSubmit.style.display = "none";
 
+        // clear outputArea
+        outputArea.innerHTML = "";
+
         // generate random num for funStats arr
         let randomNum = Math.floor(Math.random() * funStats.length);
-        funStats[randomNum];
 
         // output to outputAns
         outputAns.innerHTML = `<p>${funStats[randomNum].desc}</p><p>Source: <a href="${funStats[randomNum].sourceURL}" target="_blank">${funStats[randomNum].source}.</a></p>`;
-
-        // clear outputArea
-        outputArea.innerHTML = "";
         
         // output-area: create balls based on user numerator input
         for (let i = 0; i < funStats[randomNum].numer; i++) {
@@ -228,7 +233,7 @@ window.onload = () => {
     }
 
     // process form
-    let processForm = event => {
+    let visualize = event => {
         // prevent form submission
         event.preventDefault();
 
@@ -302,7 +307,7 @@ window.onload = () => {
     }
 
     // event listener
-    btnSubmit.addEventListener("click", processForm);
+    btnSubmit.addEventListener("click", visualize);
     btnAgain.addEventListener("click", reloadWindow);
     btnRandom.addEventListener("click", randomStat);
 }
